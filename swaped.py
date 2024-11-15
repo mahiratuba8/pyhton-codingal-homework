c4f7fa0003a800651d84746b89592569eb142274
mahiratuba8 <mahiratuba8@gmail.com>
2024-11-15 18:49:47 +0100

    Homework

---
+++ swaped.py
@@ -0,0 +1,7 @@
+NameA= str(input("Enter you first name: "))
+NameB= str(input("Ente your last name: "))
+print(f" Name before swapping is {NameA}  {NameB}")
+c=NameA
+NameA=NameB
+NameB=c
+print(f"Name after swaping is {NameA} {NameB} ")
