188b04fbf052156ad8a6bb61671194d2ef38e611
mahiratuba8 <mahiratuba8@gmail.com>
2024-12-05 19:55:18 +0100

    changes

---
+++ studentlist.py
@@ -0,0 +1,45 @@
+studentlist=["Mahira","ISGR","MYP", 10,]
+print(type(studentlist))
+dictionary={
+    "name":studentlist[0],
+    "School":studentlist[1],
+    "section":studentlist[2],
+    "grade":studentlist[3]
+}
+print(dictionary)
+studentlist2=["ALefia","ISGR","PYP", 4,]
+
+dictionary2={
+    "name":studentlist2[0],
+    "School":studentlist2[1],
+    "section":studentlist2[2],
+    "grade":studentlist2[3]
+}
+print(dictionary2)
+
+studentlist3=["Gianna","IHGR","DP", 12,]
+dictionary3={
+    "name":studentlist3[0],
+    "School":studentlist3[1],
+    "section":studentlist3[2],
+    "grade":studentlist3[3]
+}
+print(dictionary3)
+
+studentlist4=["Nehir","ISGR","MYP", 8,]
+dictionary4={
+    "name":studentlist4[0],
+    "School":studentlist4[1],
+    "section":studentlist4[2],
+    "grade":studentlist4[3]
+}
+print(dictionary4)
+
+studentlist5=["Senera","ISGR","PYP", 5,]
+dictionary5={
+    "name":studentlist5[0],
+    "School":studentlist5[1],
+    "section":studentlist5[2],
+    "grade":studentlist5[3]
+}
+print(dictionary5)
